98c9ee8b32c0d58bd46df443c7fa9ced8e3c970e
Theodore Kimana <kimana.theodore@gmail.com>
2019-01-28 19:42:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = 'Hello, Neil deGrasse Tyson!'
 
 var greetSpecialGuest = "Hello," + specialGuest + "!";
 
-var topic = "space";
+var topic = 'space'
 
-var conversation = `${topic}`;
+var conversation = Hello, Neil deGrasse Tyson!! High 5!
